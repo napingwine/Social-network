c208209d816b3ba9d7798f8aa8d5d0551c9ae41d
Andrii Pymonov <andriipymonov@gmail.com>
2021-11-06 16:21:01 +0200

    IT-KAMASUTRA REACT

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://www.pikpng.com/pngl/b/78-781995_large-size-of-all-online-degree-programs-asu.png"></img>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://img2.akspic.ru/previews/0/7/6/5/6/165670/165670-prostota-graficeskij_dizajn-materialnoe_svojstvo-purpurnyj_cvet-polutona_i_ottenki-550x310.jpg" />
+      </div>
+      <div>
+        avatar + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New Post
+        </div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
